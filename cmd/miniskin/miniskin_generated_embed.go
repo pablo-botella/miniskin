5301030c31e88bfb21a973a291eb528065f2be56
Pablo Botella <9323785+pablo-botella@users.noreply.github.com>
2026-09-14 19:18:20 +0200

    v0.5.5

--- cmd/miniskin/miniskin_generated_embed.go
+++ cmd/miniskin/miniskin_generated_embed.go
@@ -22,7 +22,7 @@ var mkskillSkill string
 var MkskillSpec = mkskill.Spec{
 	Name:        "miniskin",
 	Description: "Use when working with miniskin templates, mockup files, *.miniskin.xml configuration, or Go projects that use github.com/pablo-botella/miniskin",
-	Version:     "v0.5.3",
+	Version:     "v0.5.5",
 	Readme:      mkskillReadme,
 	Agents:      mkskillAgents,
 	Skill:       mkskillSkill,
